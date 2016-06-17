fc7b05dcaf0bee40788ebac2192452123341077c
chhe <chhe@ebay.com>
2016-06-17 17:21:00 -0400

    a

--- GameGame!/GameGame!/GameViewController.swift
+++ GameGame!/GameGame!/GameViewController.swift
@@ -12,22 +12,14 @@ import SpriteKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
+            super.viewDidLoad()
+            let scene = GameScene(size: view.bounds.size)
+            let skView = view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
+            scene.scaleMode = .ResizeFill
             skView.presentScene(scene)
-        }
     }
 
     override func shouldAutorotate() -> Bool {
